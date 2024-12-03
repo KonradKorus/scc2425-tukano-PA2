6b2ad2b0d3eacb98aec49613e2b34abe8f80068c
Jakub Smuga <jakub.smuga@weastronauts.com>
2024-12-03 16:51:00 +0000

    Add user password verification

--- scc2425-tukano/src/main/java/tukano/impl/SQLImpl/JavaUsersForSQL.java
+++ scc2425-tukano/src/main/java/tukano/impl/SQLImpl/JavaUsersForSQL.java
@@ -17,10 +17,6 @@ import tukano.impl.auth.Session;
 import utils.CSVLogger;
 import utils.SqlDB;
 
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.logging.Logger;
-
 import static java.lang.String.format;
 import static tukano.api.Result.ErrorCode.BAD_REQUEST;
 import static tukano.api.Result.ErrorCode.FORBIDDEN;
@@ -33,156 +29,152 @@ import static tukano.impl.JavaUsers.MAX_COOKIE_AGE;
 
 public class JavaUsersForSQL implements Users {
 
-	private final static String REDIS_USERS = "users:";
+    private final static String REDIS_USERS = "users:";
 
-	CSVLogger csvLogger = new CSVLogger();
-	private static Logger Log = Logger.getLogger(JavaUsersForSQL.class.getName());
-	private static Users instance;
+    CSVLogger csvLogger = new CSVLogger();
+    private static Logger Log = Logger.getLogger(JavaUsersForSQL.class.getName());
+    private static Users instance;
 
-	synchronized public static Users getInstance() {
-		if( instance == null )
-			instance = new JavaUsersForSQL();
-		return instance;
-	}
-	
-	private JavaUsersForSQL() {
-		System.out.println("==========SQLDB SOLUTION FOR USERS============");
-	}
-	
-	@Override
-	public Result<String> createUser(User user) {
-		long startTime = System.currentTimeMillis();
-		Log.info(() -> format("createUser : %s\n", user));
+    synchronized public static Users getInstance() {
+        if (instance == null)
+            instance = new JavaUsersForSQL();
+        return instance;
+    }
 
-		if( badUserInfo( user ) )
-			return error(BAD_REQUEST);
+    private JavaUsersForSQL() {
+        System.out.println("==========SQLDB SOLUTION FOR USERS============");
+    }
 
-		Result<String> result = errorOrValue( SqlDB.insertOne(user), user.getId());
-		if (result.isOK()) {
-			RedisJedisPool.addToCache(REDIS_USERS + user.getId(), user);
-		}
-		csvLogger.logToCSV("Create user ", System.currentTimeMillis() - startTime);
-		return result;
-	}
+    @Override
+    public Result<String> createUser(User user) {
+        long startTime = System.currentTimeMillis();
+        Log.info(() -> format("createUser : %s\n", user));
 
-	@Override
-	public Response getUser(String userId, String pwd) {
-		boolean pwdOk = true; // ToDo replace with code to check user password
+        if (badUserInfo(user))
+            return error(BAD_REQUEST);
 
-		if (pwdOk) {
-			String uid = UUID.randomUUID().toString();
-			var cookie = new NewCookie.Builder(COOKIE_KEY)
-					.value(uid).path("/")
-					.comment("sessionid")
-					.maxAge(MAX_COOKIE_AGE)
-					.secure(false) //ideally it should be true to only work for https requests
-					.httpOnly(true)
-					.build();
+        Result<String> result = errorOrValue(SqlDB.insertOne(user), user.getId());
+        if (result.isOK()) {
+            RedisJedisPool.addToCache(REDIS_USERS + user.getId(), user);
+        }
+        csvLogger.logToCSV("Create user ", System.currentTimeMillis() - startTime);
+        return result;
+    }
 
-			RedisJedisPool.addToCache(uid, new Session( uid, userId));
+    @Override
+    public Response getUser(String userId, String pwd) {
 
-			if (userId == null)
-				return Response.serverError().build();
+        if (userId == null)
+            return Response.serverError().build();
 
-			User cacheUser = RedisJedisPool.getFromCache(REDIS_USERS + userId, User.class);
-			if (cacheUser != null) {
-				if (cacheUser.getPwd().equals( pwd )) {
-					return Response.ok(cacheUser).cookie(cookie).build();
-				} else {
-					return Response.serverError().build();
-				}
-			}
-			var result = validatedUserOrError(SqlDB.getOne( userId, User.class), pwd);
-			return Response.ok(result).cookie(cookie).build();
-		} else {
-			throw new NotAuthorizedException("Incorrect login");
-		}
-	}
+        User user = RedisJedisPool.getFromCache(REDIS_USERS + userId, User.class);
+        if (user == null) {
+            user = SqlDB.getOne(userId, User.class).value();
+        }
 
-	@Override
-	public Result<User> getUserWithoutPwd(String userId) {
-		Log.info( () -> format("getUser : userId = %s, pwd = *no pwd*\n", userId));
+        boolean pwdOk = user.getPwd().equals(pwd);
+        if (pwdOk) {
+            String uid = UUID.randomUUID().toString();
+            var cookie = new NewCookie.Builder(COOKIE_KEY)
+                    .value(uid).path("/")
+                    .comment("sessionid")
+                    .maxAge(MAX_COOKIE_AGE)
+                    .secure(false) //ideally it should be true to only work for https requests
+                    .httpOnly(true)
+                    .build();
 
-		if (userId == null)
-			return error(BAD_REQUEST);
+            RedisJedisPool.addToCache(uid, new Session(uid, userId));
 
-		return SqlDB.getOne( userId, User.class);
-	}
+            return Response.ok(user).cookie(cookie).build();
+        } else {
+            throw new NotAuthorizedException("Incorrect login");
+        }
+    }
 
-	@Override
-	public Result<User> updateUser(String userId, String pwd, User other) {
-		long startTime = System.currentTimeMillis();
-		Log.info(() -> format("updateUser : userId = %s, pwd = %s, user: %s\n", userId, pwd, other));
+    @Override
+    public Result<User> getUserWithoutPwd(String userId) {
+        Log.info(() -> format("getUser : userId = %s, pwd = *no pwd*\n", userId));
 
-		if (badUpdateUserInfo(userId, pwd, other))
-			return error(BAD_REQUEST);
+        if (userId == null)
+            return error(BAD_REQUEST);
 
-		Result<User> result = errorOrResult( validatedUserOrError(SqlDB.getOne( userId, User.class), pwd), user -> SqlDB.updateOne( user.updateFrom(other)));
-		csvLogger.logToCSV("update user", System.currentTimeMillis() - startTime);
+        return SqlDB.getOne(userId, User.class);
+    }
 
-		if (result.isOK()) {
-			RedisJedisPool.addToCache(REDIS_USERS + userId, result.value());
-		}
+    @Override
+    public Result<User> updateUser(String userId, String pwd, User other) {
+        long startTime = System.currentTimeMillis();
+        Log.info(() -> format("updateUser : userId = %s, pwd = %s, user: %s\n", userId, pwd, other));
 
-		return result;
-	}
+        if (badUpdateUserInfo(userId, pwd, other))
+            return error(BAD_REQUEST);
 
-	@Override
-	public Result<User> deleteUser(String userId, String pwd) {
-		long startTime = System.currentTimeMillis();
-		Log.info(() -> format("deleteUser : userId = %s, pwd = %s\n", userId, pwd));
+        Result<User> result = errorOrResult(validatedUserOrError(SqlDB.getOne(userId, User.class), pwd), user -> SqlDB.updateOne(user.updateFrom(other)));
+        csvLogger.logToCSV("update user", System.currentTimeMillis() - startTime);
 
-		if (userId == null || pwd == null )
-			return error(BAD_REQUEST);
+        if (result.isOK()) {
+            RedisJedisPool.addToCache(REDIS_USERS + userId, result.value());
+        }
 
-		return errorOrResult( validatedUserOrError(SqlDB.getOne( userId, User.class), pwd), user -> {
-			// Delete user shorts and related info asynchronously in a separate thread
-			Executors.defaultThreadFactory().newThread( () -> {
-				JavaShortsForSQL.getInstance().deleteAllShorts(userId, pwd, Token.get(userId));
-			}).start();
+        return result;
+    }
 
-			Result<User> result = SqlDB.deleteOne( user);
-			csvLogger.logToCSV("delete user", System.currentTimeMillis() - startTime);
+    @Override
+    public Result<User> deleteUser(String userId, String pwd) {
+        long startTime = System.currentTimeMillis();
+        Log.info(() -> format("deleteUser : userId = %s, pwd = %s\n", userId, pwd));
 
-			if (result.isOK()) {
-				RedisJedisPool.removeFromCache(REDIS_USERS + userId);
-			}
-			return result;
-		});
-	}
+        if (userId == null || pwd == null)
+            return error(BAD_REQUEST);
 
-	@Override
-	public Result<List<User>> searchUsers(String pattern) {
-		long startTime = System.currentTimeMillis();
-		Log.info( () -> format("searchUsers : patterns = %s\n", pattern));
+        return errorOrResult(validatedUserOrError(SqlDB.getOne(userId, User.class), pwd), user -> {
+            // Delete user shorts and related info asynchronously in a separate thread
+            Executors.defaultThreadFactory().newThread(() -> {
+                JavaShortsForSQL.getInstance().deleteAllShorts(userId, pwd, Token.get(userId));
+            }).start();
 
-		var sqlQuery = format("SELECT * FROM AppUser u WHERE UPPER(u.id) LIKE '%%%s%%'", pattern.toUpperCase());
-		var sqlResult = SqlDB.sql(sqlQuery, User.class)
-				.stream()
-				.map(User::copyWithoutPassword)
-				.toList();
+            Result<User> result = SqlDB.deleteOne(user);
+            csvLogger.logToCSV("delete user", System.currentTimeMillis() - startTime);
 
-		csvLogger.logToCSV("search users", System.currentTimeMillis() - startTime);
+            if (result.isOK()) {
+                RedisJedisPool.removeFromCache(REDIS_USERS + userId);
+            }
+            return result;
+        });
+    }
+
+    @Override
+    public Result<List<User>> searchUsers(String pattern) {
+        long startTime = System.currentTimeMillis();
+        Log.info(() -> format("searchUsers : patterns = %s\n", pattern));
+
+        var sqlQuery = format("SELECT * FROM AppUser u WHERE UPPER(u.id) LIKE '%%%s%%'", pattern.toUpperCase());
+        var sqlResult = SqlDB.sql(sqlQuery, User.class)
+                .stream()
+                .map(User::copyWithoutPassword)
+                .toList();
+
+        csvLogger.logToCSV("search users", System.currentTimeMillis() - startTime);
 //		Getting cached users is obsolete since we need to get all of them from database anyway
-		List<User> cacheUsers = RedisJedisPool.getByKeyPatternFromCache(REDIS_USERS + format("*%s*", pattern.toUpperCase()), User.class);
-		List<User> cacheUsersWithoutPwd = cacheUsers.stream().map(User::copyWithoutPassword).toList();
+        List<User> cacheUsers = RedisJedisPool.getByKeyPatternFromCache(REDIS_USERS + format("*%s*", pattern.toUpperCase()), User.class);
+        List<User> cacheUsersWithoutPwd = cacheUsers.stream().map(User::copyWithoutPassword).toList();
 
-		return ok(sqlResult);
-	}
+        return ok(sqlResult);
+    }
 
-	
-	private Result<User> validatedUserOrError( Result<User> res, String pwd ) {
-		if( res.isOK())
-			return res.value().getPwd().equals( pwd ) ? res : error(FORBIDDEN);
-		else
-			return res;
-	}
-	
-	private boolean badUserInfo( User user) {
-		return (user.userId() == null || user.pwd() == null || user.displayName() == null || user.email() == null);
-	}
-	
-	private boolean badUpdateUserInfo( String userId, String pwd, User info) {
-		return (userId == null || pwd == null || info.getId() != null && ! userId.equals( info.getId()));
-	}
+
+    private Result<User> validatedUserOrError(Result<User> res, String pwd) {
+        if (res.isOK())
+            return res.value().getPwd().equals(pwd) ? res : error(FORBIDDEN);
+        else
+            return res;
+    }
+
+    private boolean badUserInfo(User user) {
+        return (user.userId() == null || user.pwd() == null || user.displayName() == null || user.email() == null);
+    }
+
+    private boolean badUpdateUserInfo(String userId, String pwd, User info) {
+        return (userId == null || pwd == null || info.getId() != null && !userId.equals(info.getId()));
+    }
 }
